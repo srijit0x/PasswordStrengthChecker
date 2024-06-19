239df3198413faf57f6972271199b413635218bb
srijit0x <163331906+srijit0x@users.noreply.github.com>
2024-06-19 13:51:43 +0200

    add readability

--- app.js
+++ app.js
@@ -1,21 +1,43 @@
 require('dotenv').config();
 
-function calculatePasswordorscheStrength(password) {
+function calculatePasswordStrength(password) {
     let strength = 0;
-    if (password.length > 8) strength += 1;
-    if (/[A-Z]/.test(password)) strength += 1;
-    if (/[a-z]/.test(password)) strength += 1;
-    if (/[0-9]/.test(password)) strength += 1;
-    if (/[^A-Za-z0-9]/.test(password)) strength += 1;
+    strength += isLengthSufficient(password);
+    strength += containsUpperCase(password);
+    strength += containsLowerCase(password);
+    strength += containsNumber(password);
+    strength += containsSpecialCharacter(password);
 
     return strength;
 }
 
+function isLengthSufficient(password) {
+    return password.length > 8 ? 1 : 0;
+}
+
+function containsUpperCase(password) {
+    return /[A-Z]/.test(password) ? 1 : 0;
+}
+
+function containsLowerCase(password) {
+    return /[a-z]/.test(password) ? 1 : 0;
+}
+
+function containsNumber(password) {
+    return /[0-9]/.test(password) ? 1 : 0;
+}
+
+function containsSpecialCharacter(password) {
+    return /[^A-Za-z0-9]/.test(password) ? 1 : 0;
+}
+
 function updateUIWithStrength(strength) {
     const strengthText = ['Very Weak', 'Weak', 'Moderate', 'Strong', 'Very Strong', 'Unbreakable'];
-    document.querySelector('#passwordStrengthDisplay').innerText = strengthText[strength];
+    const passwordStrengthDisplay = document.querySelector('#passwordStrengthDisplay');
+    passwordStrengthDisplay.innerText = strengthText[strength];
+    
     const colors = ['#ff4b4b', '#ff8c1a', '#ffd633', '#99e600', '#4dff4d'];
-    document.querySelector('#passwordStrengthDisplay').style.color = colors[strength-1] || '#ff4b4d';
+    passwordStrengthDisplay.style.color = colors[strength - 1] || '#ff4b4d';
 }
 
 function handlePasswordInput() {
